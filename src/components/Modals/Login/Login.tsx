685f979331385a5bb8d1e397827f0ae0e5a0d7dd
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-20 18:27:37 +0400

    add email form validation

--- src/components/Modals/Login/Login.tsx
+++ src/components/Modals/Login/Login.tsx
@@ -1,24 +1,62 @@
 import React from "react";
 import "./Login.scss";
+import { useForm } from "react-hook-form";
 
 type TLoginProps = {
   isLoginClicked: boolean;
   setIsLoginClicked: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+type TFormData = {
+  Email: string;
+};
 export const Login = ({ setIsLoginClicked }: TLoginProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<TFormData>();
+
+  const validateEmail = (value: string) => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@redberry\.ge$/;
+
+    return (
+      emailPattern.test(value) || "მეილი უნდა მთავრდებოდეს @redberry.ge-ით"
+    );
+  };
   return (
     <div className="login-wrapper">
       <div className="login">
         <h2>შესვლა</h2>
-        <form>
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
           <div className="login__email-container">
             <label>ელ-ფოსტა</label>
-            <input type="text" placeholder="Example@redberry.ge" />
+            <input
+              className={`email-input ${errors.Email ? "error" : ""}`}
+              {...register("Email", {
+                required: true,
+                validate: validateEmail,
+              })}
+              type="text"
+              placeholder="Example@redberry.ge"
+            />
+            {errors.Email && errors.Email.type !== "required" && (
+              <div className="email-input__error">
+                <img src="assets/svg/error-circle.svg" alt="error-circle" />
+                <span>{errors.Email.message}</span>
+              </div>
+            )}
           </div>
-          <button>შესვლა</button>
+          <button type="submit">შესვლა</button>
         </form>
       </div>
-      <img src="assets/svg/close.svg" alt="close" className="close-button" onClick={() => setIsLoginClicked(false)}/>
+      <img
+        src="assets/svg/close.svg"
+        alt="close"
+        className="close-button"
+        onClick={() => setIsLoginClicked(false)}
+      />
     </div>
   );
 };
