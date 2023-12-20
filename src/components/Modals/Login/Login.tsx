e410e31ed77ffe04761044eeb6ef69382b0866ae
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-20 15:07:48 +0400

    add login modal

---
+++ src/components/Modals/Login/Login.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Login.scss";
+
+export const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="login">
+        <h2>შესვლა</h2>
+        <form>
+          <div className="login__email-container">
+            <label>ელ-ფოსტა</label>
+            <input type="text" placeholder="Example@redberry.ge"/>
+          </div>
+          <button>შესვლა</button>
+        </form>
+      </div>
+    </div>
+  );
+};
