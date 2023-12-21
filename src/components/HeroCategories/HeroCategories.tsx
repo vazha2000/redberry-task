ba6e7908e4a84679b4092d8dcba95442952c3151
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-21 18:30:30 +0400

    render categories

--- src/components/HeroCategories/HeroCategories.tsx
+++ src/components/HeroCategories/HeroCategories.tsx
@@ -3,24 +3,49 @@ import "./HeroCategories.scss";
 import { useFetch } from "../../utils/useFetch";
 
 type TCategory = {
-  id: number,
-  title: string,
-  text_color: string,
-  background_color: string
-}
+  data: [
+    {
+      id: number;
+      title: string;
+      text_color: string;
+      background_color: string;
+    }
+  ];
+};
+
 export const HeroCategories = () => {
   const url = "https://api.blog.redberryinternship.ge/api/categories";
   const token =
     "503b1f485c12cc6d3b89177dfc9d0eb81b8d2279dc0c480dedc81a7451657268";
 
-  const [categoriesData, loading] = useFetch<TCategory[]>(
-    url, token, []
+  const initialCategoriesData: TCategory = {
+    data: [
+      {
+        id: 0,
+        title: "",
+        text_color: "",
+        background_color: "",
+      },
+    ],
+  };
+
+  const [categoriesData, loading] = useFetch<TCategory>(
+    url,
+    token,
+    initialCategoriesData
   );
 
+  console.log(categoriesData.data.map((item) => item));
 
   return (
     <div className="heroCategories-wrapper">
-      <div className="heroCategories">mkkoo</div>
+      <div className="heroCategories">
+        {categoriesData.data.map((item) => (
+          <button className="heroCategories__button" style={{color: item.text_color, backgroundColor: item.background_color}}>
+            {item.title}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
