bc06b2fbbdc99ac18fde813a48a3b7fb55b90958
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-21 18:43:48 +0400

    import HeroCategories

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./Main.scss"
 import { Navbar } from '../../components/Navbar'
 import { Hero } from '../../components/Hero'
+import { HeroCategories } from '../../components/HeroCategories'
 
 export const Main = () => {
   return (
     <div className='main-wrapper'>
       <Navbar />
       <Hero />
+      <HeroCategories />
     </div>
   )
 }
