00adc550aff6538dc1798e0ec2a81db01a5fe8e4
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-21 16:45:05 +0400

    add hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Hero.scss"
+
+export const Hero = () => {
+  return (
+    <div className='hero-wrapper'>
+      <div className="hero">
+        <h1>ბლოგი</h1>
+        <img src="assets/images/hero-image.png" alt="hero-image" />
+      </div>
+    </div>
+  )
+}
--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Main.scss"
 import { Navbar } from '../../components/Navbar'
+import { Hero } from '../../components/Hero'
 
 export const Main = () => {
   return (
     <div className='main-wrapper'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
