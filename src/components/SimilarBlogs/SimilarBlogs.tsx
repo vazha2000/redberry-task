9252392a40e5c8224c51306f408146415f8f037d
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-24 19:42:29 +0400

    pass down categories props; export type

--- src/components/SimilarBlogs/SimilarBlogs.tsx
+++ src/components/SimilarBlogs/SimilarBlogs.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SimilarBlogs.scss";
+import { TBlogsType } from "../HeroBlogs/HeroBlogs";
+import axios from "axios";
 
-export const SimilarBlogs = () => {
+type TSimilarBlogsProps = {
+  categories?: [
+    {
+      id: number;
+      title: string;
+      text_color: string;
+      background_color: string;
+    }
+  ]
+}
+export const SimilarBlogs = ({categories}: TSimilarBlogsProps) => {
+  const token =
+    "503b1f485c12cc6d3b89177dfc9d0eb81b8d2279dc0c480dedc81a7451657268";
+
+  const [blogData, setBlogData] = useState<TBlogsType>();
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.blog.redberryinternship.ge/api/blogs",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setBlogData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="similarBlogs-wrapper">
       <div className="similarBlogs">
--- src/views/BlogPage/BlogPage.tsx
+++ src/views/BlogPage/BlogPage.tsx
@@ -90,7 +90,7 @@ export const BlogPage = () => {
           </div>
         </Link>
       </div>
-      <SimilarBlogs />
+      <SimilarBlogs categories={data?.categories}/>
     </>
   );
 };
