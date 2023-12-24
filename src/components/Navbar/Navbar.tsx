c84404a36afaea2005d428eac6f83c92ac2162cc
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-24 19:24:59 +0400

    add onclick

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,13 @@ import { Login } from "../Modals/Login";
 import { Overlay } from "../Overlay";
 import { SuccessAuth } from "../Modals/SuccessAuth";
 import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
   const [isLoginClicked, setIsLoginClicked] = useState(false);
 
   const { isLoggedIn } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <div className="navbar-wrapper">
@@ -20,7 +22,7 @@ export const Navbar = () => {
           {/* {!isLoggedIn && (
           )} */}
           {isLoggedIn ? (
-            <button>დაამატე ბლოგი</button>
+            <button onClick={() => navigate("/add-blog")}>დაამატე ბლოგი</button>
           ) : (
             <button onClick={() => setIsLoginClicked(true)}>შესვლა</button>
           )}
