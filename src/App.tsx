f17d6a5951bd630b2d531ea5645338aaaac5d0c0
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-25 23:35:27 +0400

    update navbar position; update other components style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Main } from './views/Main'
-import { AddBlog } from './views/AddBlog'
+import { AddBlog2 } from './views/AddBlog'
 import { BlogPage } from './views/BlogPage'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />}/>
-      <Route path="/add-blog" element={<AddBlog />}/>
+      <Route path="/add-blog" element={<AddBlog2 />}/>
       <Route path="/blog/:id" element={<BlogPage />}/>
     </Routes>
   )
