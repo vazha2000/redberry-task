486fdc473cb0edfc9e884c08d5e93f6bb718065c
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-26 15:41:25 +0400

    update style; make props optional

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Overlay.scss";
 
 type TOverlayProps = {
-  setIsLoginClicked: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsLoginClicked?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 export const Overlay = ({ setIsLoginClicked }: TOverlayProps) => {
   return (
-    <div className="overlay" onClick={() => setIsLoginClicked(false)}>
+    <div className="overlay" onClick={() => setIsLoginClicked?.(false)}>
       
     </div>
   );
