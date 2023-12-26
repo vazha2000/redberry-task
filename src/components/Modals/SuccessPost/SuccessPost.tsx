866ae9dd9460fc46ee58fbd4511e14555a31d0f4
vazha2000 <vaja.siqtura2000@gmail.com>
2023-12-26 15:41:56 +0400

    import success post modal

--- src/components/Modals/SuccessPost/SuccessPost.tsx
+++ src/components/Modals/SuccessPost/SuccessPost.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import "./SuccessPost.scss"
+import { useNavigate } from 'react-router-dom'
 
-export const SuccessPost = () => {
+type TSuccessPostProps = {
+  setPostSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export const SuccessPost = ({setPostSuccess}: TSuccessPostProps) => {
+  const navigate = useNavigate();
   return (
     <div className="successPost-wrapper">
       <div className="successPost">
@@ -9,12 +14,13 @@ export const SuccessPost = () => {
           <img src="assets/svg/success.svg" alt="success" />
         </div>
         <h3>ჩანაწერი წარმატებით დაემატა</h3>
-        <button>მთავარ გვერდზე დაბრუნება</button>
+        <button onClick={() => {navigate("/"); setPostSuccess(false)}}>მთავარ გვერდზე დაბრუნება</button>
       </div>
       <img
         src="assets/svg/close.svg"
         alt="close"
         className="close-button"
+        onClick={() => {navigate("/"); setPostSuccess(false)}}
       />
     </div>
   )
